aa438ad71cd1ba3404ec2407bc358974b195a8bd
Omar <omaromair123@gmail.com>
2024-06-11 17:54:58 +0300

    finish the challenge

--- Frontend/src/records/card.tsx
+++ Frontend/src/records/card.tsx
@@ -4,11 +4,9 @@ import Icon from "./../icon"
 
 export default function Card({ value, unit, name, url }: cardProps): ReactElement {
 
-    console.log(`Card ${value} with ${unit} and ${name}`)
-
     return (
         <>
-            <div className="bg-white cursor-pointer flex p-3 space-x-3 items-center rounded-xl shadow-sm w-[12.5rem] lg:w-[15rem] flex-grow-0 flex-shrink-0">
+            <div className="bg-white cursor-pointer flex p-3 space-x-3 items-center rounded-xl shadow-sm w-[12.5rem] lg:w-[15rem] flex-grow-0 flex-shrink-0" >
                 <Icon url={url} size={16} />
                 <div className="text-left">
                     <h3 className="leading-8 font-semibold">{value} <span className="text-xs text-sub_gray">{unit}</span></h3>
